92b8f0bbf70fb46ab2e66ffa55eb7857dbc07028
dongwenxiao <dongwenxiao@cmcm.com>
2016-04-28 10:51:59 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,82 @@ var
     uglify     = require("gulp-uglify"),
     minifyHtml = require('gulp-minify-html'),
     minifycss  = require("gulp-minify-css"),
+    concat     = require("gulp-concat"),    
     rev        = require('gulp-rev'),
     imagemin   = require('gulp-imagemin'),
-    pngquant   = require('imagemin-pngquant');
+    pngquant   = require('imagemin-pngquant'),
+    sourcemaps = require('gulp-sourcemaps');
 
 /**
  * 自定义部署目录
  */
-var config = {
-    devDeployPath: 'dist',
-    qaDeployPath: '',
-    stageDeplyPath: ''
-} 
+
+var DEVELOP_PATH = './dist/';
+var SOURCE_PATH = './generate/';
+
+
+
+// gulp.task('minify-js', function(){
+//     return gulp.src([
+//             "./static/jquery/1.11.3/jquery.min.js",
+//             "./static/jquery.plugin/slider/1.0.0/slider.js",
+//             "./static/jquery.plugin/lazyload/1.9.5/jquery.lazyload.js",
+//             "./static/cmcm.responsive/1.0.0/cmcm.js",
+//             "./static/v/old/base.js",
+//             "./static/v/common/src/channel.js",
+//             "./static/v/old/comm-channel.js",
+//             "./static/v/common/src/fixedNav.js",
+//             "./static/v/old/scrolling.js"
+//         ])
+//         .pipe(concat("lib.min.js"))
+//         .pipe(sourcemaps.init())
+//         .pipe(uglify())
+//         .pipe(sourcemaps.write("./static/v/common/dist"))
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest(DEVELOP_PATH + "javascripts"));
+// });
+
+
+
+
+
+
 
 /**
  * 清理发布目录
  */
 gulp.task('clean', function() {
-    return gulp.src(config.devDeployPath, {read: false})
+    return gulp.src(DEVELOP_PATH, {read: false})
         .pipe(rimraf());
 });
 
 /**
  * 静态资源拷贝
  */
-gulp.task('copy-assets', function(){
-    return gulp.src(['assets/img/*'])
-        .pipe(gulp.dest('dist/assets/img'));
+// gulp.task('copy-assets', function(){
+//     return gulp.src(['./public/**/*'])
+//         .pipe(gulp.dest('dist/assets/'));
+// });
+
+/**
+ * 压缩图片并拷贝
+ */
+gulp.task('min-image', function () {
+    return gulp.src(SOURCE_PATH + 'images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(DEVELOP_PATH + 'images/'));
 });
+
 
 /**
  * 源代码压缩、合并、替换引用
  */
 gulp.task('usemin', function() {
-    return gulp.src('./*.html')
+    return gulp.src(SOURCE_PATH + 'index.html')
         .pipe(usemin({
             html: [function() { 
                 return minifyHtml({ // 压缩html
@@ -53,7 +95,7 @@ gulp.task('usemin', function() {
         }))
         .pipe(rev())  // md5后缀
         // .pipe(rev.manifest())
-        .pipe(gulp.dest(config.devDeployPath));
+        .pipe(gulp.dest(DEVELOP_PATH));
 });
 
 /**
@@ -61,7 +103,7 @@ gulp.task('usemin', function() {
  */
 gulp.task('default', ['clean'], function() {
     gulp.start('usemin');
-    gulp.start('copy-assets');
+    gulp.start('min-image');
 });
 
 
