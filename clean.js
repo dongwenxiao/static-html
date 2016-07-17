fa53e2f2901241dcb855f47c6dda4fb21162a9db
cs_victor <cs_victor@126.com>
2016-07-17 13:33:56 +0800

    delete no use

--- clean.js
+++
@@ -1,6 +0,0 @@
-var generate = require('./lib/generate');
-var GENERATE_PATH = require('./config/generate-config');
-
-generate.clean(GENERATE_PATH, function() {});
-
-console.log('Clean static finish. path:' + GENERATE_PATH);
